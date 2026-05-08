a6570389e5ce443439b5e777a63765e2cdb73029
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2026-05-08 11:52:00 +0200

    Created Classes

---
+++ Stallstjarnornas.Library/Models/Sitting.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stallstjarnornas.Library.Models
+{
+    internal class Sitting
+    {
+        public int Id { get; set; }
+        public required string Day { get; set; }
+
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+
+        public int MaxGuest { get; set; }
+
+        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+    }
+}
