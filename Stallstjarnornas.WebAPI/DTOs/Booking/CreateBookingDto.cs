b4b78b88ffaf0421563720ba0a97db3e3bfc9296
Pontus <Pontus_thoren@hotmail.com>
2026-05-12 14:35:10 +0200

    BookingDTO

---
+++ Stallstjarnornas.WebAPI/DTOs/Booking/CreateBookingDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stallstjarnornas.WebAPI.DTOs.Booking
+{
+    public record CreateBookingDto(
+        [Required, MinLength(2)]
+        string Name,
+        [Required]
+        string Phone,
+        [Required, EmailAddress]
+        string Email,
+        [Required, Range(1, 8, ErrorMessage = "Max 8 personer kan bokas online. Är ni fler, kontakta oss direkt!")]
+        int NoOfGuests,
+        [Required]
+    DateTime BookingDate,
+        [Required]
+        int SittingId,
+        string? Message
+    )
+    {
+
+    };
+}
