05a45b50a3637abb01ba82a40d6cb0c4203ce383
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2026-05-12 14:25:30 +0200

    updated seeddata

---
+++ Stallstjarnornas.WebAPI/DTOs/MailLogDTOs/MailLog.cs
@@ -0,0 +1,16 @@
+﻿using Stallstjarnornas.Library.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Stallstjarnornas.WebAPI.DTOs.MailLogDTOs
+{
+    public record CreateEmailConfirmationRequest
+    {
+        [Required] string GuestName;
+        [Required, Phone] string PhoneNo;
+        [Required, EmailAddress] string Email;
+        [Required] Booking Booking;
+        [Required] MailLog MailType;//Mailtyp skiftar beroende på bokning/avbokning/ombokning
+
+
+    }
+}
