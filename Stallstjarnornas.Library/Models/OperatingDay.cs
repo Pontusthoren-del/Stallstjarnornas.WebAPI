440fc1f9f813ea2f193420f234a370374bdc367b
Pontus <Pontus_thoren@hotmail.com>
2026-05-12 10:02:37 +0200

    Name-change

---
+++ Stallstjarnornas.Library/Models/OperatingDay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stallstjarnornas.Library.Models
+{
+    public class OperatingDay
+    {
+        public int Id { get; set; }
+
+        public DayOfWeek Day { get; set; }
+
+        public TimeOnly Opens { get; set; }
+        public TimeOnly Closes { get; set; }
+
+        public bool IsClosed { get; set; } = false;
+
+        public ICollection<Sitting> Sittings { get; set; } = new List<Sitting>();
+    }
+}
