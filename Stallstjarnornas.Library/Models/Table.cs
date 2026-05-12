9444110fbaaa358d87c5421331e01896ef622bce
Pontus <Pontus_thoren@hotmail.com>
2026-05-12 13:28:44 +0200

    Added ICollection-TableAssignments

---
+++ Stallstjarnornas.Library/Models/Table.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stallstjarnornas.Library.Models
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public int Seats { get; set; } = 2;
+        public ICollection<TableAssignment> TableAssignments { get; set; } = new List<TableAssignment>();
+
+
+    }
+}
