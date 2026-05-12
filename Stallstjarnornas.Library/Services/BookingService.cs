66483b4be4739ff4eaff9f98246d09e5afcf075f
Viktor <Viktorandersson78@gmail.com>
2026-05-12 13:57:34 +0200

    Created services

---
+++ Stallstjarnornas.Library/Services/BookingService.cs
@@ -0,0 +1,13 @@
+﻿using Stallstjarnornas.Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stallstjarnornas.Library.Services
+{
+    public class BookingService : IBookingService
+    {
+    }
+}
