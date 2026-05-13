fc11925f5b198a708e7a2eec3aa528fce817a873
Viktor <Viktorandersson78@gmail.com>
2026-05-13 14:24:36 +0200

    Made a SittingDto

---
+++ Stallstjarnornas.WebAPI/DTOs/SittingDTOs/SittingDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Stallstjarnornas.WebAPI.DTOs.SittingDTOs
+{
+    public record SittingDto(
+        
+        int Id,
+        int OperatingDayId,
+        TimeOnly StartTime,
+        TimeOnly EndTime,
+        int MaxGuest,
+        int CurrentBookings, //logig i service
+        int AvailebleSeats  //logig i service
+        );
+    
+    
+}
