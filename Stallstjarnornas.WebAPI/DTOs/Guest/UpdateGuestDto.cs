f4ad2a6b7b44f797fb629b89d6c7b96568ba3b54
Viktor <Viktorandersson78@gmail.com>
2026-05-13 08:22:52 +0200

    Created UpdateGuestDto

---
+++ Stallstjarnornas.WebAPI/DTOs/Guest/UpdateGuestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stallstjarnornas.WebAPI.DTOs.Guest
+{
+    public record UpdateGuestDto(
+
+        [Required, MinLength(2)] string Name,
+        [Required, Phone] string Phone,
+        [Required, RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+        ErrorMessage = "Ange en giltig e-postadress")] string Email,
+        [MaxLength(500)] string? Message
+    );
+
+
+}
