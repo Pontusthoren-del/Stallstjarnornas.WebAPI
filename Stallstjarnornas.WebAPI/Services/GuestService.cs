3220a019382f96d4e5fbba0fce114296558e4c22
Viktor <Viktorandersson78@gmail.com>
2026-05-15 14:28:44 +0200

    Added Get guest

--- Stallstjarnornas.WebAPI/Services/GuestService.cs
+++ Stallstjarnornas.WebAPI/Services/GuestService.cs
@@ -1,4 +1,6 @@
-﻿using Stallstjarnornas.WebAPI.Interfaces;
+﻿using Stallstjarnornas.WebAPI.Data;
+using Stallstjarnornas.WebAPI.DTOs.Guest;
+using Stallstjarnornas.WebAPI.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,26 @@ using System.Threading.Tasks;
 
 namespace Stallstjarnornas.WebAPI.Services
 {
+
     public class GuestService : IGuestService
     {
+        private readonly StallstjarnornasDbContext _context;
+
+        public GuestService(StallstjarnornasDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<GuestDto?> GetByIdAsync(int id)
+        {
+            var guest = await _context.Guests.FindAsync(id);
+            if (guest == null) return null;
+
+            return new GuestDto(
+                guest.Id,
+                guest.Name,
+                guest.Phone,
+                guest.Email
+            );
+        }
     }
 }
