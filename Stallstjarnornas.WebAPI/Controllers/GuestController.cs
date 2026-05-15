a156bbea4737b5446fac4ca7cd6363037c9bbd36
Viktor <Viktorandersson78@gmail.com>
2026-05-15 14:25:10 +0200

    Set up GetGuest

--- Stallstjarnornas.WebAPI/Controllers/GuestController.cs
+++ Stallstjarnornas.WebAPI/Controllers/GuestController.cs
@@ -1,9 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Stallstjarnornas.WebAPI.DTOs.Guest;
+using Stallstjarnornas.WebAPI.Interfaces;
 
 namespace Stallstjarnornas.WebAPI.Controllers
 {
-    public class GuestController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuestController : ControllerBase
     {
-        
+        public readonly IGuestService _service;
+
+        public GuestController(IGuestService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GuestDto>> GetGuest(int id)
+        {
+            var guest = await _service.GetByIdAsync(id);
+            if (guest == null) return NotFound();
+            return Ok(guest);
+        }
     }
 }
