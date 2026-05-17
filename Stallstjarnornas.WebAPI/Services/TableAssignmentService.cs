b5c7182d508afcb10873906411da600bcf94a557
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2026-05-17 18:14:51 +0200

    Skapat kopplingstabell för att kunna assigna flera bord till en och samma bokning. "bookingTable" heter den. Har också påbörjat TableassignmentService Har även möjliggjort demokörning av API genom att ändra launchsettings

--- Stallstjarnornas.WebAPI/Services/TableAssignmentService.cs
+++ Stallstjarnornas.WebAPI/Services/TableAssignmentService.cs
@@ -1,4 +1,9 @@
-﻿using Stallstjarnornas.WebAPI.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using Stallstjarnornas.Library.Models;
+using Stallstjarnornas.WebAPI.Data;
+using Stallstjarnornas.WebAPI.DTOs.Booking;
+using Stallstjarnornas.WebAPI.DTOs.TableAssignment;
+using Stallstjarnornas.WebAPI.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +14,37 @@ namespace Stallstjarnornas.WebAPI.Services
 {
     public class TableAssignmentService : ITableAssignmentService
     {
+        private readonly StallstjarnornasDbContext _ctx;
+
+        public TableAssignmentService(StallstjarnornasDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<TableAssignmentResponseDto> AssignTableAsync(CreateTableAssignmentDto dto)
+        {
+            var booking = await _ctx.Bookings.FirstOrDefaultAsync(b => b.Id==dto.BookingId);
+
+            if (booking == null)
+            {
+                throw new Exception("Booking not found");
+            }
+
+            var noOfGuests = booking.NoOfGuests;
+            int tablesNeeded;
+            if (noOfGuests % 2 == 0)
+            {
+                tablesNeeded = noOfGuests / 2;
+            }
+            else
+            {
+                tablesNeeded = (noOfGuests + 1) / 2;
+            }
+            //logik för att se om alla platser är bokade ligger i booking. 
+
+            
+        }
+
+
     }
 }
