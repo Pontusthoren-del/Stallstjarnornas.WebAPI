9496bc6e89093d73abc8cce26b2bd079871505c3
Viktor <Viktorandersson78@gmail.com>
2026-05-18 09:47:03 +0200

    Created method in interface

--- Stallstjarnornas.WebAPI/Interfaces/IGuestService.cs
+++ Stallstjarnornas.WebAPI/Interfaces/IGuestService.cs
@@ -1,4 +1,5 @@
 ﻿using Stallstjarnornas.WebAPI.DTOs.Guest;
+using Stallstjarnornas.Library.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,8 @@ namespace Stallstjarnornas.WebAPI.Interfaces
 {
     public interface IGuestService
     {
-        
-            Task<GuestDto?> GetGuestByIdAsync(int id);
-        
+
+        Task<GuestDto?> GetGuestByIdAsync(int id);
+        Task<Guest?> GetGustEntityByEmailAsync(string email);
     }
 }
