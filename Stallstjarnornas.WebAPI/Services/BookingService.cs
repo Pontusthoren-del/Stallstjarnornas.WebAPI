58b8c93dbddfef94a7f455d47229b20599a6a7c2
Pontus <Pontus_thoren@hotmail.com>
2026-05-18 10:51:16 +0200

    Added emailcheck

--- Stallstjarnornas.WebAPI/Services/BookingService.cs
+++ Stallstjarnornas.WebAPI/Services/BookingService.cs
@@ -15,10 +15,12 @@ namespace Stallstjarnornas.WebAPI.Services
     {
 
         private readonly StallstjarnornasDbContext _ctx;
+        private readonly IGuestService _guestService;
 
-        public BookingService(StallstjarnornasDbContext ctx)
+        public BookingService(StallstjarnornasDbContext ctx, IGuestService guestServcie)
         {
             _ctx = ctx;
+            _guestService = guestServcie;
         }
 
         public Task CancelBookingAsync(int bookingNumber)
@@ -28,7 +30,13 @@ namespace Stallstjarnornas.WebAPI.Services
 
         public async Task<BookingResponseDto> CreateBookingAsync(CreateBookingDto dto)
         {
-
+            var guest = await _guestService.GetGuestEntityByEmailAsync(dto.Email);
+            
+            if (guest == null)
+            {
+                guest = new Guest { Name = dto.Name, Phone = dto.Phone, Email = dto.Email };
+                _ctx.Guests.Add(guest); 
+            }
             //Ser till att jag hämtar bara det jag behöver från Sitting
             var sittingInfo = await _ctx.Sittings
                 .Where(s => s.Id == dto.SittingId)
@@ -55,23 +63,9 @@ namespace Stallstjarnornas.WebAPI.Services
                 throw new Exception("Sittningen är fullbokad.");
             }
 
-            var guest = await _ctx.Guests
-                .FirstOrDefaultAsync(g => g.Email == dto.Email);
-
             //Bookingtabellen -> hittar högsta bokningsnumret ->om tabellen är TOM(null) - börja på 1000.
             var maxBookingNumber = await _ctx.Bookings
-                .MaxAsync(b => (int?)b.BookingNumber) ?? 1000;
-
-            if (guest == null)
-            {
-                guest = new Guest
-                {
-                    Name = dto.Name,
-                    Phone = dto.Phone,
-                    Email = dto.Email
-                };
-                _ctx.Guests.Add(guest);
-            }
+                .MaxAsync(b => (int?)b.BookingNumber) ?? 1000;    
             //Här skapas bokningen
             var booking = new Booking
             {
@@ -102,69 +96,7 @@ namespace Stallstjarnornas.WebAPI.Services
             );
         }
 
-        public async Task<BookingResponseDto> CreateBookingExistingGuestAsync(CreateBookingExistingGuestDto dto)
-        {
-            var guest = await _ctx.Guests
-                .FirstOrDefaultAsync(g => g.Id == dto.GuestId);
-
-            if (guest == null)
-            {
-                throw new Exception("Ingen gäst hittades med det Id:t");
-            }
-
-            var sittingInfo = await _ctx.Sittings
-               .Where(s => s.Id == dto.SittingId)
-               .Select(s => new
-               {
-                   s.Id,
-                   s.StartTime,
-                   s.EndTime,
-                   s.MaxGuests,
-                   BookedGuests = s.Bookings
-                   .Where(b => b.BookingDate == dto.Date.ToDateTime(TimeOnly.MinValue)
-                   && b.Status != "Cancelled")
-                   .Sum(b => b.NoOfGuests)
-               })
-               .FirstOrDefaultAsync();
-            if (sittingInfo == null)
-                throw new Exception("Sittningen finns inte");
-
-            if (sittingInfo.BookedGuests + dto.NumberOfGuests > sittingInfo.MaxGuests)
-                throw new Exception("Sittningen är fullbokad");
-
-            var maxBokningsnummer = await _ctx.Bookings
-                .MaxAsync(b => (int?)b.BookingNumber) ?? 1000;
-
-            var booking = new Booking
-            {
-                GuestId = guest.Id,
-                SittingId = dto.SittingId,
-                BookingDate = dto.Date.ToDateTime(TimeOnly.MinValue),
-                NoOfGuests = dto.NumberOfGuests,
-                Status = "Pending",
-                BookingNumber = maxBokningsnummer + 1,
-                CreatedDate = DateTime.Now,
-                Message = dto.Message
-            };
-
-            _ctx.Bookings.Add(booking);
-            await _ctx.SaveChangesAsync();
-
-            return new BookingResponseDto(
-                BookingNumber: booking.BookingNumber,
-                GuestName: guest.Name,
-                GuestEmail: guest.Email,
-                GuestPhone: guest.Phone,
-                BookingDate: dto.Date,
-                SittingStartTime: sittingInfo.StartTime,
-                SittingEndTime: sittingInfo.EndTime,
-                NumberOfGuests: booking.NoOfGuests,
-                Status: booking.Status,
-                Message: booking.Message,
-                CreatedDate: booking.CreatedDate
-            );
-        }
-
+      
         public Task DeleteBookingAsync(int id)
         {
             throw new NotImplementedException();
