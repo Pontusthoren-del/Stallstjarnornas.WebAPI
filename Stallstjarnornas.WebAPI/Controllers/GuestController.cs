76270538723c7374f0d3ed78918423a941ec7aa6
Viktor <Viktorandersson78@gmail.com>
2026-05-18 13:00:16 +0200

    Created a Get for all the guests

--- Stallstjarnornas.WebAPI/Controllers/GuestController.cs
+++ Stallstjarnornas.WebAPI/Controllers/GuestController.cs
@@ -9,19 +9,26 @@ namespace Stallstjarnornas.WebAPI.Controllers
     [Route("api/[controller]")]
     public class GuestController : ControllerBase
     {
-        public readonly IGuestService _service;
+        private readonly IGuestService _service;
 
         public GuestController(IGuestService service)
         {
             _service = service;
         }
 
-        [HttpGet("Find-Guest{id}")]
+        [HttpGet("{id}")]
         public async Task<ActionResult<GuestDto>> GetGuest(int id)
         {
             var guest = await _service.GetGuestByIdAsync(id);
             if (guest == null) return NotFound();
             return Ok(guest);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GuestDto>>> GetAllGuests()
+        {
+            var guests = await _service.GetAllGuestsAsync();
+            return Ok(guests);
         }
     }
 }
