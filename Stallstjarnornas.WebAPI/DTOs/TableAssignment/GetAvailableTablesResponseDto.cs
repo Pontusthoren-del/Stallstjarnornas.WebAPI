3a1c2940e2f01cea0ab91c781d82d556d17e5456
DanielNilsson-GIT <daniel.gronberg92@gmail.com>
2026-05-19 15:25:27 +0200

    added getmethod for seeing available tables

---
+++ Stallstjarnornas.WebAPI/DTOs/TableAssignment/GetAvailableTablesResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Stallstjarnornas.WebAPI.DTOs.TableAssignment
+{
+    public record GetAvailableTablesResponseDto(
+
+       DateOnly bookingDate,
+       int SittingId,
+       List<int> TableIds//Admin ska kunna assigna flera bord för en bokning
+
+     );
+}
