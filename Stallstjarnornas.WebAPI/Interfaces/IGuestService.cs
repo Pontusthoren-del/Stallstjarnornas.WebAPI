6b4164591c9c4f693c9ecf27249e042d15b6a9a9
Viktor <Viktorandersson78@gmail.com>
2026-05-21 15:48:21 +0200

    Created way to UpdateGuestAync

--- Stallstjarnornas.WebAPI/Interfaces/IGuestService.cs
+++ Stallstjarnornas.WebAPI/Interfaces/IGuestService.cs
@@ -1,16 +1,12 @@
 ﻿using Stallstjarnornas.WebAPI.DTOs.Guest;
 using Stallstjarnornas.Library.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Stallstjarnornas.WebAPI.Interfaces
 {
     public interface IGuestService
     {
-
+        Task<GuestDto?> UpdateGuestAsync(int id, UpdateGuestDto dto);
         Task<GuestDto?> GetGuestByIdAsync(int id);
         Task<IEnumerable<GuestDto>> GetAllGuestsAsync();
         Task<Guest?> GetGuestEntityByEmailAsync(string email);
