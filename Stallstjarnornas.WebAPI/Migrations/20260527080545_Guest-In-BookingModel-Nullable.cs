322df56652347562200842ecedfabbf487e8172c
Viktor <Viktorandersson78@gmail.com>
2026-05-27 10:16:14 +0200

    Made GuestId Nullable

---
+++ Stallstjarnornas.WebAPI/Migrations/20260527080545_Guest-In-BookingModel-Nullable.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Stallstjarnornas.WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class GuestInBookingModelNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Guests_GuestId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GuestId",
+                table: "Bookings",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Guests_GuestId",
+                table: "Bookings",
+                column: "GuestId",
+                principalTable: "Guests",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Guests_GuestId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GuestId",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Guests_GuestId",
+                table: "Bookings",
+                column: "GuestId",
+                principalTable: "Guests",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
