cbab88e9469f651859f8c57828e2e4adb539cafc
Pontus <Pontus_thoren@hotmail.com>
2026-06-01 11:31:49 +0200

    Update UpdateBookingDto

--- Stallstjarnornas.WebAPI/DTOs/Booking/UpdateBookingDto.cs
+++ Stallstjarnornas.WebAPI/DTOs/Booking/UpdateBookingDto.cs
@@ -1,7 +1,10 @@
-﻿namespace Stallstjarnornas.WebAPI.DTOs.Booking
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stallstjarnornas.WebAPI.DTOs.Booking
 {
     public record UpdateBookingDto(
         DateOnly? BookingDate,
+        [Range(1, 8, ErrorMessage = "Max 8 personer kan bokas online. Är ni fler, kontakta oss direkt!")]
         int? NumberOfGuests,
         int? SittingId,
         string? Status,
