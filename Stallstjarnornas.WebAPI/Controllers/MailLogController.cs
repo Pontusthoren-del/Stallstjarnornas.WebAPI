9a855168d7ee6057115ab76c2a92e8a61c162f1f
Pontus <Pontus_thoren@hotmail.com>
2026-06-01 11:31:33 +0200

    Added MailLogController

---
+++ Stallstjarnornas.WebAPI/Controllers/MailLogController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stallstjarnornas.WebAPI.Interfaces;
+
+namespace Stallstjarnornas.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MailLogController : ControllerBase
+    {
+        private readonly IMailLogService _service;
+
+        public MailLogController(IMailLogService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetMailLog()
+        {
+            var logs = await _service.GetMailLogAsync();
+            return Ok(logs);
+        }
+    }
+}
